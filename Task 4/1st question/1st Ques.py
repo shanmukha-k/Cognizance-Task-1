687b663b36444d9c403b647cbbd8607766963f17
shannu21032 <99649721+shannu21032@users.noreply.github.com>
2022-02-23 15:26:44 +0530

    Delete Task 4 directory

--- Task 4/1st question/1st Ques.py
+++
@@ -1,4 +0,0 @@
-num1=int(input("First Number: "))
-num2=int(input("Second Number: "))
-sum=num1+num2
-print(f'Sum = {sum}')
--- Task 4/2nd question/2nd ques.py
+++
@@ -1,7 +0,0 @@
-msg = input("Enter the string: ")
-num = (len(msg))
-ans = ""
-for i in range(0, num):
-    if i % 2 == 0:
-        ans = ans + msg[i]
-print(ans)
